a81b7b2fc0404cad5ce94035adfd1ebae8e0ac37
Jorge <Jorge@Jorge-PC>
2014-02-26 21:35:53 +0100

    He creat la pantalla del buscaminas y li he ficat un lletrero y textura, ja no se fer res més jajajajajaj

---
+++ JuankerVIP/src/buscaminas/Game.java
@@ -0,0 +1,32 @@
+package buscaminas;
+
+import fge.Color;
+import fge.Font;
+import fge.Render;
+import fge.Service;
+import fge.Texture;
+import fge.Window;
+
+public class Game extends Service {
+
+	private Font font;
+
+	private Texture texBackground;
+
+	public void onStart() {
+
+		texBackground = new Texture("PNG", "data/grey_wash_wall512.png");
+
+		// Crec que aci es on se te que crear el tablero y generar la posicio
+		// aleatoria de les bombes, pero ni idea...
+
+		font = new Font("data/COMIC.ttf", 48.0f);
+
+	}
+
+	public void onDraw() {
+		Render.DrawTex(texBackground, 0, 0, Window.getW(), Window.getH(),
+				new Color(255, 255, 255), 0, 0, 800 / 350.f, 600 / 259.f);
+		font.Draw(16, 16, "Buscaminas", new Color(255, 255, 0));
+	}
+}
