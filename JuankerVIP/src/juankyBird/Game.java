f71cc4633dd26bd51e977e46c9261fae85b3ec84
adripc64 <adripc64@gmail.com>
2014-02-27 13:06:43 +0100

    paquete para juankyBird

---
+++ JuankerVIP/src/juankyBird/Game.java
@@ -0,0 +1,23 @@
+package juankyBird;
+
+import fge.Service;
+
+public class Game extends Service {
+	
+	public void onStart() {
+		
+	}
+	
+	public void onStop() {
+		
+	}
+
+	public void onMove() {
+		
+	}
+	
+	public void onDraw() {
+		
+	}
+
+}
