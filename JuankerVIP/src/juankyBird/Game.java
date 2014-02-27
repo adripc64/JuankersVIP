5fcf5ee9c62d58d07b294627f981d938e7e9ec71
Jorge <Jorge@Jorge-PC>
2014-02-27 17:51:58 +0100

    He ficat un background de un cel, pero is descuadrat

--- JuankerVIP/src/juankyBird/Game.java
+++ JuankerVIP/src/juankyBird/Game.java
@@ -6,82 +6,90 @@ import fge.Event;
 import fge.EventMan;
 import fge.Render;
 import fge.Service;
+import fge.Texture;
 import fge.Window;
 
 public class Game extends Service {
-	
+
 	private Pardal pardal;
 	private float aceleracion;
-	
+	private Texture texBackground;
+
 	@Override
 	public void onStart() {
-		
+
 		aceleracion = 0.0f;
 		pardal = new Pardal();
-		pardal.setAltura(Window.getH()/2.0f);
-		
+		pardal.setAltura(Window.getH() / 2.0f);
+
+		texBackground = new Texture("PNG", "data/paisaje.png");
+
 		EventMan.addListener(this);
 	}
-	
+
 	@Override
 	public void onStop() {
-		
+
 	}
-	
+
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
-	public void onMove() {//constante descendente y implementacion de  onUp
-	
-		if(pardal.getAltura() >= 0){
+	public void onMove() {// constante descendente y implementacion de onUp
+
+		if (pardal.getAltura() >= 0) {
 			aceleracion += 750.0f * App.getFTime();
-			if (aceleracion > 250) aceleracion = 250.0f;
+			if (aceleracion > 250)
+				aceleracion = 250.0f;
 			pardal.setAltura(pardal.getAltura() - aceleracion * App.getFTime());
 		}
-		if(pardal.getAltura() < 0){
+		if (pardal.getAltura() < 0) {
 			pardal.setAltura(0);
 		}
-		if(pardal.getAltura() > Window.getH()-64){
-			pardal.setAltura(Window.getH()-64);
+		if (pardal.getAltura() > Window.getH() - 64) {
+			pardal.setAltura(Window.getH() - 64);
 		}
 	}
 
 	@Override
 	public void onDraw() {
-		
+
 		int w = pardal.getTextura().getW();
 		int h = pardal.getTextura().getH();
 		float x = (Window.getW() - w) / 2.0f;
 		float y = Window.getH() - h - pardal.getAltura();
-		
-		Render.DrawTex(pardal.getTextura(), x, y, w, h, new Color(255, 255, 255));
 
+		Render.DrawTex(texBackground, 0, 0, Window.getW(), Window.getH(),
+				new Color(255, 255, 255));
+		Render.DrawTex(pardal.getTextura(), x, y, w, h,
+				new Color(255, 255, 255));
 	}
-	
+
 	public boolean doEvent(Event e) {
-		
-		if (e.getType() == Event.MOUSE_PRESSED || e.getType() == Event.KEY_PRESSED) {
-			
-			//int mx = Mouse.getX();
-			//int my = Mouse.getY();
-			
+
+		if (e.getType() == Event.MOUSE_PRESSED
+				|| e.getType() == Event.KEY_PRESSED) {
+
+			// int mx = Mouse.getX();
+			// int my = Mouse.getY();
+
 			// Ací es cuan li has de donar el empuje...
 			aceleracion = -400.0f;
-			
+
 		}
-		
+
 		return false;
-		
+
 	}
 
 }
