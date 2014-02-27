56858d83815763fc3572298d5de1c0b97e29827a
adripc64 <adripc64@gmail.com>
2014-02-27 14:05:51 +0100

    Cambis de Jorge al buscaminas

---
+++ JuankerVIP/src/buscaminas/Board.java
@@ -0,0 +1,36 @@
+package buscaminas;
+
+import fge.Color;
+import fge.Render;
+import fge.Texture;
+
+public class Board {
+
+	private Texture celdaNoPulsada = new Texture("PNG",
+			"data/piece_white64.png");;
+	// private Texture celdaPulsada;
+	// private Texture mina;
+	int boardCells;
+	int xCells;
+	int yCells;
+	int x;
+	int y;
+
+	public Board(int x, int y) {
+		xCells = x;
+		yCells = y;
+		boardCells = x * y;
+	}
+
+	public void draw() {
+		for (int i = 0; i < xCells; i++) {
+			x = i * 64;
+			for (int j = 0; j < yCells; j++) {
+				y = j * 64;
+				Render.DrawTex(celdaNoPulsada, x, y, 64, 64, new Color(255,
+						255, 255));
+			}
+		}
+
+	}
+}
--- JuankerVIP/src/buscaminas/Game.java
+++ JuankerVIP/src/buscaminas/Game.java
@@ -10,28 +10,24 @@ import fge.Window;
 public class Game extends Service {
 
 	private Font font;
-
+	private Board board;
 	private Texture texBackground;
-	
+
 	private Minefield minefield;
 
-	
 	public void onStart() {
 
 		texBackground = new Texture("PNG", "data/grey_wash_wall512.png");
-
-		// Crec que aci es on se te que crear el tablero y generar la posicio
-		// aleatoria de les bombes, pero ni idea...
-		minefield = new Minefield(16,16,20);
+		board = new Board(10, 10);
+		minefield = new Minefield(16, 16, 20);
 		System.out.println(minefield);
-
 		font = new Font("data/COMIC.ttf", 48.0f);
-
 	}
 
 	public void onDraw() {
 		Render.DrawTex(texBackground, 0, 0, Window.getW(), Window.getH(),
 				new Color(255, 255, 255), 0, 0, 800 / 350.f, 600 / 259.f);
+		board.draw();
 		font.Draw(16, 16, "Buscaminas", new Color(255, 255, 0));
 	}
 }
