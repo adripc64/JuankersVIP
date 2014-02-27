55426a46d2b44ee5fd6f06ea2cae5b8bdb799c08
aragonet <carles aragon@carlesaragon-PC>
2014-02-27 13:19:11 +0100

    carles

--- JuankerVIP/src/juankyBird/Game.java
+++ JuankerVIP/src/juankyBird/Game.java
@@ -3,7 +3,10 @@ package juankyBird;
 import fge.Service;
 
 public class Game extends Service {
-	
+	private int esquinaX;
+	private int esquinaY;
+	private int largo;
+	private int ancho;
 	public void onStart() {
 		
 	}
@@ -12,7 +15,13 @@ public class Game extends Service {
 		
 	}
 
-	public void onMove() {
+	public void onMove() {//constante descendente y implementacion de  onUp
+		
+	}
+	public void onUp(){//per a cuan pulses cap a dalt
+		
+	}
+	public void onDown(){//per a no parar de baixar fins tocar terra.
 		
 	}
 	
