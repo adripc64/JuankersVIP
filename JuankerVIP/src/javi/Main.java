bb05eccf01d8c44600d1525fdb6cfe4b1dce8da9
Javier <Javier@192.168.1.131>
2014-02-28 17:07:53 +0100

    Paquete nuevo

---
+++ JuankerVIP/src/javi/Main.java
@@ -0,0 +1,49 @@
+package javi;
+
+import fge.App;
+import fge.Service;
+
+public class Main extends Service{
+	public static Main main = new Main();
+	
+	public static void main(String[] args) {
+		App.run(main, 640, 640);
+	}
+	
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onMove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onDraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
