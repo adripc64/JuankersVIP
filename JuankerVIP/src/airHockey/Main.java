828daa486f3fea378c33e573ba1fe84348f154a2
Javier <Javier@192.168.1.131>
2014-02-28 17:26:05 +0100

    rfj

---
+++ JuankerVIP/src/airHockey/Main.java
@@ -0,0 +1,43 @@
+package airHockey;
+
+import fge.Service;
+
+public class Main extends Service{
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onMove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onDraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
