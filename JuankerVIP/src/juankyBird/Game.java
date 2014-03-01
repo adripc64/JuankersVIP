ef1609a8afd2154806adac50463c309e5b4810b0
aragonet <carles aragon@carlesaragon-PC>
2014-03-01 12:32:27 +0100

    show tuberias constantment

--- JuankerVIP/src/juankyBird/Game.java
+++ JuankerVIP/src/juankyBird/Game.java
@@ -15,11 +15,13 @@ public class Game extends Service implements EventListener {
 
 	private Pardal pardal;
 	private Tuberia tuberia;
+	private Tuberia tuberia2;
 	private float aceleracion;
-	private float tubSpeed = 200.0f;
+	private float tubSpeed = 250.0f;
 	private Texture texBackground;
 	private float backgroundUX = 0.0f;
-	private float backgroundSpeed = 30.0f;
+	private float backgroundSpeed = 40.0f;
+	private boolean contador=false;
 	
 	private Texture texTuboCuerpo;
 	private Texture texTuboCuerpoInv;
@@ -29,6 +31,7 @@ public class Game extends Service implements EventListener {
 		aceleracion = 0.0f;
 		pardal = new Pardal();
 		tuberia=new Tuberia();
+		tuberia2=new Tuberia();
 		tuberia.setX(Window.getW()+tuberia.getTextura().getW());
 
 		texBackground = new Texture("PNG", "data/paisaje.png"); // a�o es molt xapussa sa darreglar
@@ -85,6 +88,20 @@ public class Game extends Service implements EventListener {
 			float tuby = (float) (Math.random() * (Window.getH()-tuberia.getSeparacion())-tuberia.getSeparacion()-100);
 			tuberia.setYT(tuby);
 		}
+		//tuberia 2
+		
+		if((tuberia.getX()<=(Window.getW()/2+tuberia.getTextura().getW()/2)) && contador==false){
+			contador=true;
+		}
+		if(tuberia2.getX() > 0-tuberia2.getTextura().getW()&&contador==true){
+			float tubx2 = tuberia2.getX() - tubSpeed * App.getFTime();
+			tuberia2.setX(tubx2);
+		} else {
+			tuberia2.setX(Window.getH()+tuberia2.getTextura().getW()+50);
+			float tuby2 = (float) (Math.random() * (Window.getH()-tuberia2.getSeparacion())-tuberia.getSeparacion()-100);
+			tuberia2.setYT(tuby2);
+		}
+		
 	}
 
 	@Override
@@ -92,9 +109,7 @@ public class Game extends Service implements EventListener {
 		Render.DrawTex(texBackground, 0, 0, Window.getW(), Window.getH(),
 				new Color(255, 255, 255), backgroundUX, 0.0f, 1.0f, 1.0f);
 
-		// Render.DrawTex(texTuboCuerpo, 0, 0, 128, 512, new Color(255,255,255), 0, 0, 1, 512/8.f);
-		
-		// Dibujando tuber�a 
+		// Dibujando tuberia 
 		int wT=tuberia.getTextura().getW();
 		int hT=tuberia.getTextura().getH();
 		float xT=tuberia.getX(); // Constante
@@ -105,6 +120,19 @@ public class Game extends Service implements EventListener {
 		yT+=hT+tuberia.getSeparacion();
 		Render.DrawTex(tuberia.getTextura(), xT, yT, wT, hT, new Color(255, 255, 255));
 		Render.DrawTex(texTuboCuerpo, xT, Window.getH(), wT,yT-Window.getH()+80, new Color(255, 255, 255));
+		//tuberia2
+		int wT2=tuberia2.getTextura().getW();
+		int hT2=tuberia2.getTextura().getH();
+		float xT2=tuberia2.getX(); // Constante
+		float yT2=tuberia2.getYT();
+		Render.DrawTex(tuberia2.getTexturaInv(), xT2, yT2, wT2, hT2, new Color(255, 255, 255));
+		Render.DrawTex(texTuboCuerpoInv, xT2, 0, wT2, yT2+hT2-70, new Color(255, 255, 255));
+			
+		yT2+=hT2+tuberia2.getSeparacion();
+		Render.DrawTex(tuberia2.getTextura(), xT2, yT2, wT2, hT2, new Color(255, 255, 255));
+		Render.DrawTex(texTuboCuerpo, xT2, Window.getH(), wT2,yT2-Window.getH()+80, new Color(255, 255, 255));
+		
+		
 		// Dibujando pajaro
 		int w = pardal.getTextura().getW();
 		int h = pardal.getTextura().getH();
