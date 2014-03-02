f93414f7748795e5edc390676b441ba0ba15532c
Javier <Javier@Javier>
2014-03-02 12:02:28 +0100

    Empezando colisiones...

--- JuankerVIP/src/airHockey/Game.java
+++ JuankerVIP/src/airHockey/Game.java
@@ -1,5 +1,6 @@
 package airHockey;
 
+import fge.App;
 import fge.Color;
 import fge.Event;
 import fge.EventListener;
@@ -16,10 +17,16 @@ public class Game extends Service implements EventListener {
 	private Ball ball;
 	private Mazo mazo1;
 //	private Mazo mazo2;
+	private float acceleracionX;
+	private float acceleracionY;
 	
 	
 	@Override
 	public void onStart() {
+		
+		acceleracionX = 0.0f;
+		acceleracionY = 0.0f;
+		
 		// Fondo de pantalla
 		texBackground = new Texture("data/airhockey/campo.png");
 		
@@ -30,8 +37,10 @@ public class Game extends Service implements EventListener {
 		
 		// Mazo 1
 		mazo1 = new Mazo();
-		mazo1.setxMazo(Window.getW() / 4 - mazo1.getTex().getW() / 2);
-		mazo1.setyMazo(Window.getH() / 2 - mazo1.getTex().getH() / 2);
+		mazo1.setxMazo(Window.getW() / 4.0f - mazo1.getTex().getW() / 2);
+		mazo1.setyMazo(Window.getH() / 2.0f - mazo1.getTex().getH() / 2);
+		
+		
 		EventMan.addListener(this);
 	}
 
@@ -63,12 +72,28 @@ public class Game extends Service implements EventListener {
 	}
 	
 	private void moveBall(){
-		if(ball.getyBall() < 0) {					// Si se sale por la parte de ARRIBA de la pantalla
+		if(ball.getyBall() < 0) {										// Parte de ARRIBA de la pantalla
 			// Debe rebotar...
 		}
-		if(ball.getyBall() > Window.getH()) {		// Si se sale por la parte de ABAJO de la pantalla
+		if(ball.getyBall() > Window.getH() - ball.getTex().getH()) {	// Parte de ABAJO de la pantalla
 			// Debe rebotar...
 		}
+		if(ball.getxBall() - ball.getTex().getW() > Window.getW()) {	// Parte DERECHA de la pantalla
+			ball.setxBall(Window.getW() - ball.getTex().getW());
+			acceleracionX = 0.0f;
+		}
+		if(ball.getxBall() < 0)	{										// Parte IZQUIERDA de la pantalla
+			ball.setxBall(0);
+			acceleracionX = 0.0f;
+		}
+		
+		// Choca el mazo 1 con la pelota por la DERECHA
+		if( mazo1.getxMazo() == (ball.getxBall() - (ball.getTex().getW() / 2)) )
+			acceleracionX = 50.0f;
+
+		// Moviendo la pelota
+		ball.setxBall(ball.getxBall() + acceleracionX * App.getFTime());	// Componente X
+		ball.setyBall(ball.getyBall() + acceleracionY * App.getFTime());	// Componente Y
 	}
 	
 	private void moveMazo1(){
@@ -77,15 +102,19 @@ public class Game extends Service implements EventListener {
 			mazo1.setxMazo(mazo1.getxMazo() - 5.0f);
 		if(Keyboard.isKeyPressed(Keyboard.KEY_RIGHT))
 			mazo1.setxMazo(mazo1.getxMazo() + 5.0f);
+		if(Keyboard.isKeyPressed(Keyboard.KEY_UP))
+			mazo1.setyMazo(mazo1.getyMazo() - 5.0f);
+		if(Keyboard.isKeyPressed(Keyboard.KEY_DOWN))
+			mazo1.setyMazo(mazo1.getyMazo() + 5.0f);
 		
-		if(mazo1.getxMazo() < 0)					// Si se sale por la parte izquierda de la pantalla
+		if(mazo1.getxMazo() < 0)										// Parte izquierda de la pantalla
 			mazo1.setxMazo(0);
-		if(mazo1.getxMazo()+mazo1.getTex().getW() > Window.getW() / 2)	// Si se sale de su campo
+		if(mazo1.getxMazo() + mazo1.getTex().getW() > Window.getW()/2)	// Si se sale de su CAMPO
 			mazo1.setxMazo(Window.getW() / 2 - mazo1.getTex().getW());
-		if(mazo1.getyMazo() < 0)					// Si se sale por la parte de ARRIBA de la pantalla
+		if(mazo1.getyMazo() < 0)										// Parte de ARRIBA de la pantalla
 			mazo1.setyMazo(0);
-		if(mazo1.getyMazo() > Window.getH())		// Si se sale por la parte de ABAJO de la pantalla
-			mazo1.setyMazo(Window.getH());
+		if(mazo1.getyMazo() > Window.getH() - mazo1.getTex().getH())	// Si se sale por la parte de ABAJO de la pantalla
+			mazo1.setyMazo(Window.getH() - mazo1.getTex().getH());
 	}
 
 	@Override
@@ -96,8 +125,8 @@ public class Game extends Service implements EventListener {
 		// Dibujando pelota
 		int wB = ball.getTex().getW();
 		int hB = ball.getTex().getH();
-		float xB = (Window.getW() / 2.0f) - (ball.getTex().getW() / 2);
-		float yB = (Window.getH() / 2.0f) - (ball.getTex().getH() / 2);
+		float xB = ball.getxBall();
+		float yB = ball.getyBall();
 		Render.DrawTexture(ball.getTex(), xB, yB, wB, hB, 0, new Color(255, 255, 255));
 		
 		// Dibujando mazo 1
