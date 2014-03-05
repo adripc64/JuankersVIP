3482d633c43fd02351554e7c984fd4b89e185604
aragonet <carles aragon@carlesaragon-PC>
2014-03-05 17:42:45 +0100

    inici Animació pardal

---
+++ JuankerVIP/src/juankyBird/Animacio.java
@@ -0,0 +1,51 @@
+package juankyBird;
+
+import fge.Event;
+import fge.EventListener;
+import fge.Service;
+
+public class Animacio extends Service implements EventListener{
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onMove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onDraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean doEvent(Event e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
