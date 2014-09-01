3f50c0418ca804c346b125ad0dec04531af9b252
Elite Legend <Elite Legend@192.168.1.35>
2014-09-01 20:17:54 +0200

    Gravity

--- JuankerVIP/src/qubeOnline/Main.java
+++ JuankerVIP/src/qubeOnline/Main.java
@@ -1,5 +1,7 @@
 package qubeOnline;
 
+import org.omg.CORBA.TIMEOUT;
+
 import fge.App;
 import fge.Color;
 import fge.Keyboard;
@@ -8,11 +10,18 @@ import fge.Service;
 
 public class Main extends Service {
 	float x = 100;
-	float y = 100;
+	float y = 500;
 	int w = 50;
 	int h = 50;
 	Color color = new Color(0, 255, 0);
-	float speed = 50;
+	float speed = 100;
+	boolean falling = true;
+	float gravity = 300;
+	float jumpSpeed = 500;
+	final float MAX_JUMP_HEIGHT = 300;
+	boolean jump = false;
+	float jumpingHeight = 0;
+	
 	
 	
 	
@@ -49,15 +58,37 @@ public class Main extends Service {
 
 	@Override
 	protected void onMove() {
-		if (Keyboard.isKeyPressed(Keyboard.KEY_LEFT)) x -= speed * App.getFTime();
-		if (Keyboard.isKeyPressed(Keyboard.KEY_RIGHT)) x += speed * App.getFTime();
-		if (Keyboard.isKeyPressed(Keyboard.KEY_UP)) y -= speed * App.getFTime();
-		if (Keyboard.isKeyPressed(Keyboard.KEY_DOWN)) y += speed * App.getFTime();
+		if (!Keyboard.isKeyPressed(Keyboard.KEY_UP)){
+			falling = true;
+			jumpingHeight = 0;
+		}
+		if (falling && y < 650) y += gravity * App.getFTime();
+		if (Keyboard.isKeyPressed(Keyboard.KEY_LEFT) && x > 0) x -= speed * App.getFTime();
+		if (Keyboard.isKeyPressed(Keyboard.KEY_RIGHT) && x < 550 ) x += speed * App.getFTime();
+		if (Keyboard.isKeyPressed(Keyboard.KEY_UP) && jump == false ) {
+			falling = false;
+			if (jumpingHeight <= MAX_JUMP_HEIGHT){
+				y -= jumpSpeed * App.getFTime();
+				jumpingHeight = 650-y;
+				System.out.println(jumpingHeight);
+				}
+			}
+//			falling = false;
+//			if(jumping <= MAX_JUMP_HEIGHT) {
+//				
+//				System.out.println(jumping);
+//			}
+//			else {
+//				jump = false;
+
+//			}
 	}
+
 
 	@Override
 	protected void onDraw() {
 		Render.DrawFilledRectangle(x, y, w, h, color);
+		Render.DrawFilledRectangle(0, 700, 600, 10, color);
 	}
 
 }
