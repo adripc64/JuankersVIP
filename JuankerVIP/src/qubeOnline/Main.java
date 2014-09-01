be07fa1333f11b376ed1baa4d35522d73611e8e5
adripc64 <adripc64@gmail.com>
2014-09-01 20:44:01 +0200

    Gravedad mejorada y código más limpio

--- JuankerVIP/src/qubeOnline/Main.java
+++ JuankerVIP/src/qubeOnline/Main.java
@@ -1,94 +1,109 @@
 package qubeOnline;
 
-import org.omg.CORBA.TIMEOUT;
-
 import fge.App;
 import fge.Color;
+import fge.Event;
+import fge.EventListener;
+import fge.EventMan;
 import fge.Keyboard;
 import fge.Render;
 import fge.Service;
+import fge.Window;
 
-public class Main extends Service {
-	float x = 100;
-	float y = 500;
-	int w = 50;
-	int h = 50;
-	Color color = new Color(0, 255, 0);
-	float speed = 100;
-	boolean falling = true;
-	float gravity = 300;
-	float jumpSpeed = 500;
-	final float MAX_JUMP_HEIGHT = 300;
-	boolean jump = false;
-	float jumpingHeight = 0;
+public class Main extends Service implements EventListener {
 	
+	final float horizontalSpeed = 300.0f;
+	final float jumpSpeed = 600.0f;
+	final float gravity = 1200.0f;
+		
+	float playerX = 100;
+	float playerY = 300;
+	int playerW = 50;
+	int playerH = 50;
+	Color playerColor = new Color(0, 255, 0);
+	float playerSpeedX = 0.0f;
+	float playerSpeedY = 0.0f;
+	boolean playerFalling = true;
 	
-	
+	float groundX = 0;
+	float groundY = 600;
+	int groundW = 600;
+	int groundH = 10;
+	Color groundColor = new Color(0, 255, 0);
 	
 	public static void main(String[] args) {
 		App.run(new Main(), 600, 800);
-
-
 	}
 
 	@Override
 	protected void onStart() {
-		// TODO Auto-generated method stub
-		
+		EventMan.addListener(this);
 	}
 
 	@Override
 	protected void onStop() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected void onPause() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected void onResume() {
 		
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	protected void onMove() {
-		if (!Keyboard.isKeyPressed(Keyboard.KEY_UP)){
-			falling = true;
-			jumpingHeight = 0;
-		}
-		if (falling && y < 650) y += gravity * App.getFTime();
-		if (Keyboard.isKeyPressed(Keyboard.KEY_LEFT) && x > 0) x -= speed * App.getFTime();
-		if (Keyboard.isKeyPressed(Keyboard.KEY_RIGHT) && x < 550 ) x += speed * App.getFTime();
-		if (Keyboard.isKeyPressed(Keyboard.KEY_UP) && jump == false ) {
-			falling = false;
-			if (jumpingHeight <= MAX_JUMP_HEIGHT){
-				y -= jumpSpeed * App.getFTime();
-				jumpingHeight = 650-y;
-				System.out.println(jumpingHeight);
-				}
-			}
-//			falling = false;
-//			if(jumping <= MAX_JUMP_HEIGHT) {
-//				
-//				System.out.println(jumping);
-//			}
-//			else {
-//				jump = false;
+		
+		// Gravedad
+		if (playerFalling) playerSpeedY += gravity * App.getFTime();
+		
+		playerX += playerSpeedX * App.getFTime(); // Movimiento horizontal
+		playerY += playerSpeedY * App.getFTime(); // Movimiento vertical
 
-//			}
+		// Evita que se salga por los laterales de la ventana
+		if (playerX < 0) playerX = 0;
+		if (playerX + playerW > Window.getW()) playerX = Window.getW() - playerW;
+		// Evita que pase del suelo
+		if (playerY + playerH > groundY) {
+			playerY = groundY - playerH;
+			playerSpeedY = 0.0f;
+			playerFalling = false;
+		}
+		
 	}
 
 
 	@Override
 	protected void onDraw() {
-		Render.DrawFilledRectangle(x, y, w, h, color);
-		Render.DrawFilledRectangle(0, 700, 600, 10, color);
+		Render.DrawFilledRectangle(playerX, playerY, playerW, playerH, playerColor);
+		Render.DrawFilledRectangle(groundX, groundY, groundW, groundH, groundColor);
+	}
+
+	@Override
+	public boolean doEvent(Event e) {
+
+		switch (e.getType()) {
+			case KEY_PRESSED:
+				if (e.getValue() == Keyboard.KEY_LEFT) playerSpeedX -= horizontalSpeed;
+				if (e.getValue() == Keyboard.KEY_RIGHT) playerSpeedX += horizontalSpeed;
+				if (e.getValue() == Keyboard.KEY_UP && !playerFalling) {
+					playerFalling = true;
+					playerSpeedY -= jumpSpeed; 
+				}
+				break;
+			case KEY_RELEASED:
+				if (e.getValue() == Keyboard.KEY_LEFT) playerSpeedX += horizontalSpeed;
+				if (e.getValue() == Keyboard.KEY_RIGHT) playerSpeedX -= horizontalSpeed;
+				break;
+			default:
+				break;
+		}
+		
+		return false;
 	}
 
 }
